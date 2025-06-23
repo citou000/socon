018e317f5a602275238b9f887ba3bce19e9f5333
citou000 <citoujean@gmail.com>
2025-06-23 14:48:41 +0000

    feat: initialize Vue 3 project with Tailwind CSS and basic routing
    - Added pnpm workspace configuration for built dependencies.
    - Created favicon.ico for the application.
    - Set up main App component with RouterView.
    - Imported Tailwind CSS in main stylesheet.
    - Configured main.js to create and mount Vue app with router.
    - Implemented router with a HomeView component.
    - Developed HomeView component with a simple dashboard layout.
    - Configured Vite for Vue and Tailwind CSS integration.

---
+++ socon-front/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const dashboardOption = ref({
+  'Baptisés': 50,
+  'Âmes': 100,
+  'Suivis': 50});
+
+
+</script>
+
+<template>
+  <main class="w-full h-screen bg-purple-100">
+    <div class="h-[40%] bg-purple-200 text-purple-900 flex">
+      <div class="w-full flex items-center justify-around h-full px-8">
+        <div v-for="(k, v) in dashboardOption" :key="dashboardItem" class="flex flex-col items-start justify-around w-fit cursor-pointer bg-white p-8">
+        <span class="self-start text-md">{{ v }}</span>
+        <span class="text-9xl">{{ k }}</span>
+      </div>
+      </div>
+    </div>
+    <div class="flex items-center justify-center">
+      This is the table place
+    </div>
+  </main>
+</template>
