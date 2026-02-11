92b73fd7e3e6f12fd0f98d1453bc30ea319b4a88
citou000 <citoujean@gmail.com>
2026-02-11 22:51:07 +0000

    This is fucking slow

--- src/components/AddTeam.vue
+++ src/components/AddTeam.vue
@@ -1,48 +1,50 @@
 <script setup>
+// import ref from 'vue';
 import BaseButton from '@/components/BaseButton.vue';
 import { X } from 'lucide-vue-next';
+// import { supabase } from '@/lib/supabaseClient';
+import { useAuth } from '@/store/auth';
+import { storeToRefs } from "pinia"
 
 const emit = defineEmits(['close']);
+
+const authStore = useAuth();
+
+const { user } = storeToRefs(authStore);
+
+// const teamName = ref();
+
+
+const submitTeam = async () => {
+  console.log("User from addTeam components:", user.value.id)
+  // const { data, error } = await supabase().from('teams').insert({
+  //   name: teamName.value,
+  //   admin_id: user.value.id,
+  // })
+  // if ()
+}
 </script>
 <template>
   <div class="fixed inset-0 bg-gray-700/50 backdrop-blur-sm flex justify-center items-center z-40">
-    <div class="flex flex-col items-start px-6 pt-4 pb-9 bg-white rounded-lg max-w-md md:w-[40%]">
-      <div class="w-full mb-4 flex justify-between items-center">
+    <div class="flex flex-col items-start px-6 pt-4 pb-9 bg-white rounded-lg max-w-md md:w-[40%]
+      mx-4">
+      <div class="w-full mb-4 flex justify-between items-center gap-2">
         <section>
-          <h2 class="text-3xl font-bold">Ajouter une équipe</h2>
-          <p class="text-sm">
-            Les champs marqués par <span class="text-red-500">*</span> sont obligatoires
-          </p>
+          <h2 class="md:text-3xl font-bold text-3xl">Ajouter une équipe</h2>
         </section>
-        <section>
-          <X
-            class="size-6 cursor-pointer hover:text-red-500 font-bold transition-colors duration-100"
-            @click="$emit('close')"
-          />
+        <section class="place-self-start md:place-self-center mt-2">
+          <X class="size-6 cursor-pointer hover:text-red-500 font-bold transition-colors duration-100"
+            @click="emit('close')" />
         </section>
       </div>
-      <form class="w-full flex flex-col gap-4">
+      <form class="w-full flex flex-col gap-4" @submit.prevent>
         <div class="flex flex-col self-center w-full">
           <label for="team-name">Nom de l'équipe <sup class="text-red-500">*</sup></label>
-          <input
-            type="text"
-            id="team-name"
-            name="team-name"
-            required
-            class="border-1 border-purple-200 px-1 py-2 rounded-md"
-          />
+          <input type="text" id="team-name" name="team-name" required
+            class="border-1 border-purple-200 px-1 py-2 rounded-md" />
         </div>
 
-        <div class="flex flex-col">
-          <label for="team-description">Description de l'équipe</label>
-          <textarea
-            id="team-description"
-            name="team-description"
-            class="border-1 border-purple-200 px-1 py-2 rounded-md w-full resize-none columns-1"
-          ></textarea>
-        </div>
-
-        <BaseButton type="submit" variant="primary">Créer l'équipe</BaseButton>
+        <BaseButton type="submit" variant="primary" @click='submitTeam'>Créer l'équipe</BaseButton>
       </form>
     </div>
   </div>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -4,13 +4,13 @@ import BaseButton from '@/components/BaseButton.vue';
 import NavBar from '@/components/NavBar.vue';
 import { Search, Plus } from 'lucide-vue-next';
 import TeamCard from '@/components/TeamCard.vue';
-import { useMemberStore } from '@/store/member';
+// import { useMemberStore } from '@/store/member';
 import AddTeam from '@/components/AddTeam.vue';
+import { supabase } from '@/lib/supabaseClient';
 
-const memberStore = useMemberStore();
+// const memberStore = useMemberStore();
 const isEmpty = ref(false);
 const isClosed = ref(true);
-
 const closeModal = () => {
   isClosed.value = true;
 };
@@ -26,23 +26,13 @@ const closeModal = () => {
       <h2 class="text-5xl font-bold mb-8">Vos équipes</h2>
 
       <div class="flex items-center justify-between gap-4">
-        <div
-          class="flex items-center bg-gray-100 border border-gray-300 rounded-lg px-3 py-2 w-full max-w-sm focus-within:border-purple-400 transition-all"
-        >
-          <Search size="18" class="text-gray-500 mr-2" />
-          <input
-            type="text"
-            placeholder="Rechercher"
-            class="bg-transparent border-none outline-none w-full text-sm"
-          />
-        </div>
+        <!-- <div -->
+        <!--   class="flex items-center bg-gray-100 border border-gray-300 rounded-lg px-3 py-2 w-full max-w-sm focus-within:border-purple-400 transition-all"> -->
+        <!--   <Search size="18" class="text-gray-500 mr-2" /> -->
+        <!--   <input type="text" placeholder="Rechercher" class="bg-transparent border-none outline-none w-full text-sm" /> -->
+        <!-- </div> -->
 
-        <BaseButton
-          variant="primary"
-          :width="false"
-          class="whitespace-nowrap"
-          @click="isClosed = false"
-        >
+        <BaseButton variant="primary" :width="false" class="whitespace-nowrap" @click="isClosed = false">
           <Plus />
           Ajouter une équipe
         </BaseButton>
@@ -51,12 +41,11 @@ const closeModal = () => {
       <div class="mt-8 h-full">
         <div
           class="mt-8 border-2 border-dashed border-gray-200 rounded-xl h-64 flex items-center justify-center text-gray-400"
-          v-if="isEmpty"
-        >
+          v-if="isEmpty">
           Pas d'équipe pour le moment
         </div>
 
-        <div v-else class="grid grid-cols-3 gap-8">
+        <div v-else class="grid md:grid-cols-3 md:gap-8 gap-2">
           <TeamCard name="Équipe Alpha" description="Description de l'équipe Alpha" />
           <TeamCard name="EJP Adidogomé" description="L'église des jeunes prodiges d'adidogomé" />
         </div>
